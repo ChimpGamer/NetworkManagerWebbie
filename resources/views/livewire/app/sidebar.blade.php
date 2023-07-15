30e30afffa8ed92b4758549799f8b268c7e1c6b2
ChimpGamer <Tim.Cremers@Ziggo.nl>
2023-07-15 20:13:41 +0200

    Set correct icons in sidebar

--- resources/views/livewire/app/sidebar.blade.php
+++ resources/views/livewire/app/sidebar.blade.php
@@ -7,19 +7,19 @@
             <a href="analytics" class="list-group-item list-group-item-action py-2 ripple @if(Str::startsWith(Route::currentRouteName(), 'analytics')) active @endif"><i
                     class="fas fa-chart-line fa-fw me-3"></i><span>Analytics</span></a>
             <a href="#" class="list-group-item list-group-item-action py-2 ripple">
-                <i class="fas fa-chart-area fa-fw me-3"></i><span>Players</span>
+                <i class="fas fa-users fa-fw me-3"></i><span>Players</span>
             </a>
             <a href="punishments" class="list-group-item list-group-item-action py-2 ripple @if(Str::startsWith(Route::currentRouteName(), 'punishments')) active @endif">
                 <i class="fas fa-lock fa-fw me-3"></i><span>Punishments</span>
             </a>
             <a href="announcements" class="list-group-item list-group-item-action py-2 ripple @if(Str::startsWith(Route::currentRouteName(), 'announcements')) active @endif">
-                <i class="fas fa-lock fa-fw me-3"></i><span>Announcements</span>
+                <i class="fas fa-bullhorn fa-fw me-3"></i></i><span>Announcements</span>
             </a>
             <a href="servers" class="list-group-item list-group-item-action py-2 ripple @if(Str::startsWith(Route::currentRouteName(), 'servers')) active @endif">
-                <i class="fas fa-lock fa-fw me-3"></i><span>Servers</span>
+                <i class="fas fa-server fa-fw me-3"></i><span>Servers</span>
             </a>
             <a href="punishment_templates" class="list-group-item list-group-item-action py-2 ripple @if(Str::startsWith(Route::currentRouteName(), 'punishment_templates')) active @endif">
-                <i class="fas fa-lock fa-fw me-3"></i><span>Punishment Templates</span>
+                <i class="fas fa-file-lines fa-fw me-3"></i><span>Punishment Templates</span>
             </a>
         </div>
     </div>
