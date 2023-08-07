a7d6ac6a476c7d352ea14ad3950d51a75cbeec5c
ChimpGamer <Tim.Cremers@Ziggo.nl>
2023-08-07 17:47:31 +0200

    Disable addLanguage button because it doesn't work yet.

--- resources/views/livewire/languages/show-languages.blade.php
+++ resources/views/livewire/languages/show-languages.blade.php
@@ -59,7 +59,7 @@
     </div>
     <div class="p-4">
         <button type="button" class="btn btn-primary" data-mdb-toggle="modal" data-mdb-target="#addLanguageModal"
-                wire:click="addLanguage">
+                wire:click="addLanguage" disabled>
             <i style="vertical-align: bottom;font-size: 18px !important;" class="material-icons">add</i> Add Language
         </button>
     </div>
