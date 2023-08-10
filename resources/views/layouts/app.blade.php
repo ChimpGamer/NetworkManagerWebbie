5a46dc64b0107b0bca6136b3e942ea3725599273
ChimpGamer <Tim.Cremers@Ziggo.nl>
2023-08-10 18:36:59 +0200

    Added Popper for nicer tooltips

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,8 @@
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"
         integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
 
+    <script defer src="https://unpkg.com/@popperjs/core@2"></script>
+
     @stack('styles')
 </head>
 
