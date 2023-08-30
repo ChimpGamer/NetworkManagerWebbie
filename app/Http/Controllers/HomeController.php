84106ca734796fb528b4c27ad24281424625cae9
ChimpGamer <Tim.Cremers@Ziggo.nl>
2023-08-30 16:47:47 +0200

    Added 4 stats to the dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Player;
+use Carbon\Carbon;
+use Carbon\CarbonInterval;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
+
     /**
      * Create a new controller instance.
      *
@@ -16,6 +21,31 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
+    private function getTotalPlayers(): int {
+        return Player::count();
+    }
+
+    private function getTodayOnlinePlayers(): int {
+        $start = Carbon::today()->getTimestampMs();
+        return Player::where('lastlogin', '>', $start)->count();
+    }
+
+    private function getNewPlayers(): int {
+        $start = Carbon::today()->getTimestampMs();
+        return Player::where('firstlogin', '>', $start)->count();
+    }
+
+    private function getTodayPlaytime(): string {
+        $start = Carbon::today()->getTimestampMs();
+        $data = DB::table('sessions')->select('time')->where('start', '>', $start)->get();
+
+        $time = 0;
+        foreach ($data as $item) {
+            $time += $item->time;
+        }
+        return date('H:i:s', $time / 1000);
+    }
+
     /**
      * Show the application dashboard.
      *
@@ -23,6 +53,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home')
+            ->with('total_players', $this->getTotalPlayers())
+            ->with('today_online_players', $this->getTodayOnlinePlayers())
+            ->with('new_players', $this->getNewPlayers())
+            ->with('today_playtime', $this->getTodayPlaytime())
+            ;
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,68 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
     <div class="row justify-content-center">
+        <div class="col-xl-3 col-sm-6 col-12 mb-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between px-md-1">
+                        <div class="align-self-center">
+                            <i class="fas fa-users text-info fa-3x"></i>
+                        </div>
+                        <div class="text-end">
+                            <h3>{{ $total_players }}</h3>
+                            <p class="mb-0">Total Players</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 col-12 mb-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between px-md-1">
+                        <div class="align-self-center">
+                            <i class="fas fa-user-check text-warning fa-3x"></i>
+                        </div>
+                        <div class="text-end">
+                            <h3>{{ $today_online_players }}</h3>
+                            <p class="mb-0">Today Online Players</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 col-12 mb-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between px-md-1">
+                        <div class="align-self-center">
+                            <i class="fas fa-user-plus text-success fa-3x"></i>
+                        </div>
+                        <div class="text-end">
+                            <h3>{{ $new_players }}</h3>
+                            <p class="mb-0">Today New Players</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 col-12 mb-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between px-md-1">
+                        <div class="align-self-center">
+                            <i class="fas fa-user-clock text-danger fa-3x"></i>
+                        </div>
+                        <div class="text-end">
+                            <h3>{{ $today_playtime }}</h3>
+                            <p class="mb-0">Today Playtime</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
@@ -20,5 +80,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
