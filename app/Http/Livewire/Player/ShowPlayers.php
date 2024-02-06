046d4cf5feaac63ea5b61ca750d870bffe6443ec
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-02-06 22:02:48 +0100

    Allow searching player by uuid

--- app/Http/Livewire/Player/ShowPlayers.php
+++ app/Http/Livewire/Player/ShowPlayers.php
@@ -12,19 +12,22 @@ class ShowPlayers extends Component
     use WithPagination;
 
     protected string $paginationTheme = 'bootstrap';
+
     public string $search = '';
 
-    public function updated() {
+    public function updated()
+    {
         $this->resetPage();
     }
 
-
     public function render(): View
     {
-        $players = Player::where('username', 'like', '%' . $this->search . '%')
+        $players = Player::where('username', 'like', '%'.$this->search.'%')
             ->orWhere('ip', '=', $this->search)
+            ->orWhere('uuid', '=', $this->search)
             ->orderBy('firstlogin', 'DESC')
             ->paginate(10);
+
         return view('livewire.players.show-players')->with('players', $players);
     }
 }
