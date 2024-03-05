f25fdbfb81758146f8fe1e2455055c334f9a608b
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-03-05 22:16:42 +0100

    Added support for dark theme to the login page.

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-mdb-theme="{{ config('app.theme') }}">
 
 <head>
     <meta charset="UTF-8">
