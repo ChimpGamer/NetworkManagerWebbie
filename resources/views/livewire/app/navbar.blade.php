affbd0b9275dc6750371e8333da97215763d2f80
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-03-05 15:05:30 +0100

    Tell as which user you are signed in at the profile dropdown menu.

--- resources/views/livewire/app/navbar.blade.php
+++ resources/views/livewire/app/navbar.blade.php
@@ -3,14 +3,14 @@
     <div class="container-fluid">
         <!-- Toggle button -->
         <button class="navbar-toggler" type="button" data-mdb-collapse-init data-mdb-target="#sidebarMenu"
-            aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <i class="fas fa-bars"></i>
         </button>
 
         <!-- Brand -->
         <a class="navbar-brand">
             <img src="/images/full_logo.png" height="25" alt="NetworkManager Logo"
-                loading="lazy" />
+                 loading="lazy"/>
         </a>
 
         <!-- Right links -->
@@ -18,14 +18,23 @@
             <!-- Avatar -->
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle hidden-arrow d-flex align-items-center" href="#"
-                    id="navbarDropdownMenuLink" role="button" data-mdb-dropdown-init aria-expanded="false">
+                   id="navbarDropdownMenuLink" role="button" data-mdb-dropdown-init aria-expanded="false">
                     @if (Auth::check())
-                        <img src="https://minotar.net/helm/{{ Auth::user()->getUUID() }}" class="rounded-circle" height="22" alt="Avatar" loading="lazy" />
-                     @else
-                        <img src="https://minotar.net/helm/MHF_Steve" class="rounded-circle" height="22" alt="Avatar" loading="lazy" />
+                        <img src="https://minotar.net/helm/{{ Auth::user()->getUUID() }}" class="rounded-circle"
+                             height="22" alt="Avatar" loading="lazy"/>
+                    @else
+                        <img src="https://minotar.net/helm/MHF_Steve" class="rounded-circle" height="22" alt="Avatar"
+                             loading="lazy"/>
                     @endif
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+                    <li>
+                        <div class="dropdown-header" style="color: inherit !important;">
+                            <p style="font-size:0.875rem;line-height: 1.25rem;margin:0;">Signed in as</p>
+                            <p style="font-weight: 500; font-size:0.875rem;line-height: 1.25rem;margin:0;">{{ Auth::user()->username }}</p>
+                        </div>
+                    </li>
+                    <li><hr class="dropdown-divider"></li>
                     <li>
                         <a class="dropdown-item" href="/profile">My profile</a>
                     </li>
