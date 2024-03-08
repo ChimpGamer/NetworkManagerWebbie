4b2917592fe85f695d0c399b1adfd4c4561dcd63
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-03-08 14:14:58 +0100

    Improved getTodayPlaytime method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -41,12 +41,7 @@ class HomeController extends Controller
     private function getTodayPlaytime(): string
     {
         $start = Carbon::today()->getTimestampMs();
-        $data = DB::table('sessions')->select('time')->where('start', '>', $start)->get();
-
-        $time = 0;
-        foreach ($data as $item) {
-            $time += $item->time;
-        }
+        $time = DB::table('sessions')->select('time')->where('start', '>', $start)->sum('time');
         try {
             return CarbonInterval::millisecond($time)->cascade()->forHumans(['short' => true, 'options' => 0]);
         } catch (\Exception $ex) {
