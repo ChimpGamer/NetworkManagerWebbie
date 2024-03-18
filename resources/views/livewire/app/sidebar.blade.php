331461502c6b88f60d7b4cf00fac0f9a70967f4f
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-03-18 17:48:33 +0100

    Make sidebar scrollable

--- resources/views/livewire/app/sidebar.blade.php
+++ resources/views/livewire/app/sidebar.blade.php
@@ -1,5 +1,5 @@
 <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-body">
-    <div class="position-sticky">
+    <div class="sidebar-sticky">
         <div class="list-group list-group-flush mx-3 mt-4">
             <a href="/"
                data-mdb-ripple-init
@@ -21,32 +21,40 @@
                 </a>
             @endcan
             @can('view_punishments')
-                <a href="/punishments"
-                   data-mdb-ripple-init
-                   class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'punishments')) active @endif">
-                    <i class="fas fa-lock fa-fw me-3"></i><span>Punishments</span>
-                </a>
+                @if($this->isModuleEnabled('module_punishments'))
+                    <a href="/punishments"
+                       data-mdb-ripple-init
+                       class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'punishments')) active @endif">
+                        <i class="fas fa-lock fa-fw me-3"></i><span>Punishments</span>
+                    </a>
+                @endif
             @endcan
             @can('view_announcements')
-                <a href="/announcements"
-                   data-mdb-ripple-init
-                   class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'announcements')) active @endif">
-                    <i class="fas fa-bullhorn fa-fw me-3"></i><span>Announcements</span>
-                </a>
+                @if($this->isModuleEnabled('module_announcements'))
+                    <a href="/announcements"
+                       data-mdb-ripple-init
+                       class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'announcements')) active @endif">
+                        <i class="fas fa-bullhorn fa-fw me-3"></i><span>Announcements</span>
+                    </a>
+                @endif
             @endcan
             @can('view_servers')
-                <a href="/servers"
-                   data-mdb-ripple-init
-                   class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'servers')) active @endif">
-                    <i class="fas fa-server fa-fw me-3"></i><span>Servers</span>
-                </a>
+                @if($this->isModuleEnabled('module_servers'))
+                    <a href="/servers"
+                       data-mdb-ripple-init
+                       class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'servers')) active @endif">
+                        <i class="fas fa-server fa-fw me-3"></i><span>Servers</span>
+                    </a>
+                @endif
             @endcan
             @can('view_pre_punishments')
-                <a href="/punishment_templates"
-                   data-mdb-ripple-init
-                   class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'punishment_templates')) active @endif">
-                    <i class="fas fa-file-lines fa-fw me-3"></i><span>Punishment Templates</span>
-                </a>
+                @if($this->isModuleEnabled('module_pre_punishments'))
+                    <a href="/punishment_templates"
+                       data-mdb-ripple-init
+                       class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'punishment_templates')) active @endif">
+                        <i class="fas fa-file-lines fa-fw me-3"></i><span>Punishment Templates</span>
+                    </a>
+                @endif
             @endcan
             @can('view_languages')
                 <a href="/languages"
@@ -56,32 +64,40 @@
                 </a>
             @endcan
             @can('view_permissions')
-                <a href="/permissions"
-                   data-mdb-ripple-init
-                   class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'permissions')) active @endif">
-                    <i class="fas fa-lock fa-fw me-3"></i><span>Permissions</span>
-                </a>
+                @if($this->isModuleEnabled('module_permissions_bungee'))
+                    <a href="/permissions"
+                       data-mdb-ripple-init
+                       class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'permissions')) active @endif">
+                        <i class="fas fa-lock fa-fw me-3"></i><span>Permissions</span>
+                    </a>
+                @endif
             @endcan
             @can('view_filter')
-                <a href="/filter"
-                   data-mdb-ripple-init
-                   class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'filter')) active @endif">
-                    <i class="fas fa-filter-circle-xmark fa-fw me-3"></i><span>Filter</span>
-                </a>
+                @if($this->isModuleEnabled('module_filter'))
+                    <a href="/filter"
+                       data-mdb-ripple-init
+                       class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'filter')) active @endif">
+                        <i class="fas fa-filter-circle-xmark fa-fw me-3"></i><span>Filter</span>
+                    </a>
+                @endif
             @endcan
             @can('view_commandblocker')
-                <a href="/commandblocker"
-                   data-mdb-ripple-init
-                   class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'commandblocker')) active @endif">
-                    <i class="fas fa-ban fa-fw me-3"></i><span>Command Blocker</span>
-                </a>
+                @if($this->isModuleEnabled('module_commandblocker'))
+                    <a href="/commandblocker"
+                       data-mdb-ripple-init
+                       class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'commandblocker')) active @endif">
+                        <i class="fas fa-ban fa-fw me-3"></i><span>Command Blocker</span>
+                    </a>
+                @endif
             @endcan
             @can('view_helpop')
-                <a href="/helpop"
-                   data-mdb-ripple-init
-                   class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'helpop')) active @endif">
-                    <i class="fas fa-question fa-fw me-3"></i><span>HelpOP</span>
-                </a>
+                @if($this->isModuleEnabled('module_helpop'))
+                    <a href="/helpop"
+                       data-mdb-ripple-init
+                       class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'helpop')) active @endif">
+                        <i class="fas fa-question fa-fw me-3"></i><span>HelpOP</span>
+                    </a>
+                @endif
             @endcan
             @can('view_chat')
                 <a href="/chat"
@@ -91,25 +107,31 @@
                 </a>
             @endcan
             @can('view_tags')
-                <a href="/tags"
-                   data-mdb-ripple-init
-                   class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'tags')) active @endif">
-                    <i class="fas fa-tags fa-fw me-3"></i><span>Tags</span>
-                </a>
+                @if($this->isModuleEnabled('module_tags'))
+                    <a href="/tags"
+                       data-mdb-ripple-init
+                       class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'tags')) active @endif">
+                        <i class="fas fa-tags fa-fw me-3"></i><span>Tags</span>
+                    </a>
+                @endif
             @endcan
             @can('view_tickets')
-                <a href="/tickets"
-                   data-mdb-ripple-init
-                   class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'tickets')) active @endif">
-                    <i class="fas fa-ticket fa-fw me-3"></i><span>Tickets</span>
-                </a>
+                @if($this->isModuleEnabled('module_tickets'))
+                    <a href="/tickets"
+                       data-mdb-ripple-init
+                       class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'tickets')) active @endif">
+                        <i class="fas fa-ticket fa-fw me-3"></i><span>Tickets</span>
+                    </a>
+                @endif
             @endcan
             @can('view_network')
-                <a href="/motd"
-                   data-mdb-ripple-init
-                   class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'motd')) active @endif">
-                    <i class="fas fa-message fa-fw me-3"></i><span>MOTD</span>
-                </a>
+                @if($this->isModuleEnabled('motd_enabled'))
+                    <a href="/motd"
+                       data-mdb-ripple-init
+                       class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'motd')) active @endif">
+                        <i class="fas fa-message fa-fw me-3"></i><span>MOTD</span>
+                    </a>
+                @endif
                 <a href="/settings"
                    data-mdb-ripple-init
                    class="list-group-item list-group-item-action py-2 @if(Str::startsWith(Route::currentRouteName(), 'settings')) active @endif">
