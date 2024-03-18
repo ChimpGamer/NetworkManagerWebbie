b8757baaa1c487ff7aedf6d084b3ed8640dc0d48
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-03-18 18:36:54 +0100

    Check if module is enabled for item in sidebar

--- app/Livewire/Sidebar.php
+++ app/Livewire/Sidebar.php
@@ -2,11 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Models\Value;
 use Illuminate\View\View;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Sidebar extends Component
 {
+
+    #[Computed]
+    public function isModuleEnabled(string $moduleName): bool
+    {
+        return Value::where('variable', $moduleName)->where('value', 1)->exists();
+    }
+
     public function render(): View
     {
         return view('livewire.app.sidebar');
