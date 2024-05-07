a3d93c903fc81f106cd8435bcb86decd63c2c096
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-05-07 16:46:13 +0200

    Get all enabled values on page load instead of checking every individual one with the database.

--- app/Livewire/Sidebar.php
+++ app/Livewire/Sidebar.php
@@ -3,17 +3,24 @@
 namespace App\Livewire;
 
 use App\Models\Value;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\View\View;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Sidebar extends Component
 {
+    public Collection $values;
+
+    public function mount(): void
+    {
+        $this->values = Value::where('value', 1)->get();
+    }
 
     #[Computed]
     public function isModuleEnabled(string $moduleName): bool
     {
-        return Value::where('variable', $moduleName)->where('value', 1)->exists();
+        return $this->values->where('variable', $moduleName)->isNotEmpty();
     }
 
     public function render(): View
