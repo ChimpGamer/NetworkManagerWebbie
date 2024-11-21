c2e5daf261ea8061ff35a917ebde923412056348
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-11-21 15:45:54 +0100

    Forgot the PunishmentsTable class

---
+++ app/Livewire/PunishmentsTable.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Punishment;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Blade;
+use PowerComponents\LivewirePowerGrid\Button;
+use PowerComponents\LivewirePowerGrid\Column;
+use PowerComponents\LivewirePowerGrid\Facades\PowerGrid;
+use PowerComponents\LivewirePowerGrid\PowerGridComponent;
+use PowerComponents\LivewirePowerGrid\PowerGridFields;
+
+final class PunishmentsTable extends PowerGridComponent
+{
+    public string $tableName = 'punishments-table';
+
+    public string $sortDirection = 'desc';
+
+    public function setUp(): array
+    {
+        return [
+            PowerGrid::header()
+                ->showSearchInput(),
+            PowerGrid::footer()
+                ->showPerPage()
+                ->showRecordCount(),
+        ];
+    }
+
+    public function datasource(): Builder
+    {
+        return Punishment::query();
+    }
+
+    public function fields(): PowerGridFields
+    {
+        return PowerGrid::fields()
+            ->add('id_label', function ($item) {
+                $id = $item->id;
+                if ($item->active) {
+                    return '<i class="fas fa-xmark-circle fa-lg text-danger"></i> '.$id;
+                } else {
+                    return '<i class="fas fa-check-circle fa-lg text-success"></i> '.$id;
+                }
+            })
+            ->add('type_name', fn ($item) => $item->type->name)
+            ->add('player', fn ($item) => Blade::render('<x-player-link uuid="'.$item->uuid.'" username="'.$item->getPlayerName().'" />'))
+            ->add('punisher', fn ($item) => $item->getPunisherName())
+            ->add('time', fn ($item) => $item->getTimeFormatted())
+            ->add('reason');
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make('ID', 'id_label', 'id')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Type', 'type_name', 'type')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Player', 'player', 'uuid')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Punisher', 'punisher')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Time', 'time')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Reason', 'reason')
+                ->sortable()
+                ->searchable(),
+
+            Column::action('Action'),
+        ];
+    }
+
+    public function actions(Punishment $row): array
+    {
+        return [
+            Button::add('info')
+                ->attributes(['data-mdb-ripple-init' => '', 'data-mdb-modal-init' => '', 'data-mdb-target' => '#showPunishmentModal'])
+                ->slot('<i class="material-icons text-info">info</i>')
+                ->can(auth()->user()->can('view_punishments'))
+                ->id()
+                ->class('bg-transparent border-0')
+                ->dispatch('info', ['rowId' => $row->id]),
+            Button::add('edit')
+                ->attributes(['data-mdb-ripple-init' => '', 'data-mdb-modal-init' => '', 'data-mdb-target' => '#editPunishmentModal'])
+                ->slot('<i class="material-icons text-warning">edit</i>')
+                ->can(auth()->user()->can('edit_punishments'))
+                ->id()
+                ->class('bg-transparent border-0')
+                ->dispatch('edit', ['rowId' => $row->id]),
+            Button::add('delete')
+                ->attributes(['data-mdb-ripple-init' => '', 'data-mdb-modal-init' => '', 'data-mdb-target' => '#deletePunishmentModal'])
+                ->slot('<i class="material-icons text-danger">delete</i>')
+                ->can(auth()->user()->can('edit_punishments'))
+                ->id()
+                ->class('bg-transparent border-0')
+                ->dispatch('delete', ['rowId' => $row->id]),
+        ];
+    }
+
+    /*
+    public function actionRules($row): array
+    {
+       return [
+            // Hide button edit for ID 1
+            Rule::button('edit')
+                ->when(fn($row) => $row->id === 1)
+                ->hide(),
+        ];
+    }
+    */
+}
