c69b8401018b95fb46dc6499690af463a5a90190
ChimpGamer <tim.cremers@ziggo.nl>
2024-11-27 09:50:23 +0100

    Fix toggle columns dropdown

---
+++ resources/views/vendor/livewire-powergrid/components/frameworks/bootstrap5/header/toggle-columns.blade.php
@@ -0,0 +1,36 @@
+<div class="dropdown">
+    @if (data_get($setUp, 'header.toggleColumns'))
+        <div class="btn-group">
+            <button
+                id="dropdown"
+                class="btn btn-secondary dropdown-toggle"
+                type="button"
+                data-mdb-dropdown-init
+                data-mdb-ripple-init
+                aria-expanded="false"
+            >
+                <x-livewire-powergrid::icons.eye-off width="18" />
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdown">
+                @foreach ($this->visibleColumns as $column)
+                    <li
+                        wire:click="$dispatch('pg:toggleColumn-{{ $tableName }}', { field: '{{ data_get($column, 'field') }}'})"
+                        wire:key="toggle-column-{{ data_get($column, 'field') }}"
+                    >
+                        <a
+                            class="dropdown-item"
+                            href="#"
+                        >
+                            @if (data_get($column, 'hidden') === false)
+                                <x-livewire-powergrid::icons.eye width="18" />
+                            @else
+                                <x-livewire-powergrid::icons.eye-off width="18" />
+                            @endif
+                            {!! data_get($column, 'title') !!}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+</div>
