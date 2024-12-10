ba12d89023c5619e169a341a84e19d7d688ec4c8
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-12-10 10:55:26 +0100

    Updated Permission Group Suffixes to use powergrid

--- app/Livewire/Permissions/ShowGroupSuffixes.php
+++ app/Livewire/Permissions/ShowGroupSuffixes.php
@@ -6,15 +6,12 @@ use App\Models\Permissions\Group;
 use App\Models\Permissions\GroupSuffix;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\View\View;
+use Livewire\Attributes\On;
 use Livewire\Component;
-use Livewire\WithPagination;
 
 class ShowGroupSuffixes extends Component
 {
-    use WithPagination;
     use AuthorizesRequests;
-
-    protected string $paginationTheme = 'bootstrap';
 
     public ?int $suffixId;
 
@@ -24,7 +21,7 @@ class ShowGroupSuffixes extends Component
 
     public Group $group;
 
-    protected function rules()
+    protected function rules(): array
     {
         return [
             'suffix' => 'required|string',
@@ -32,20 +29,12 @@ class ShowGroupSuffixes extends Component
         ];
     }
 
-    public function updated($fields)
-    {
-        $this->validateOnly($fields);
-        if ($fields == 'search') {
-            $this->resetPage();
-        }
-    }
-
     public function addGroupSuffix(): void
     {
         $this->resetInput();
     }
 
-    public function createGroupSuffix()
+    public function createGroupSuffix(): void
     {
         $validatedData = $this->validate();
         $server = empty($validatedData['server']) ? '' : $validatedData['server'];
@@ -60,16 +49,23 @@ class ShowGroupSuffixes extends Component
         $this->closeModal('addGroupSuffixModal');
     }
 
-    public function editGroupSuffix(GroupSuffix $groupSuffix)
+    #[On('edit')]
+    public function editGroupSuffix($rowId): void
     {
         $this->resetInput();
+        $groupSuffix = GroupSuffix::find($rowId);
+        if ($groupSuffix == null) {
+            session()->flash('error', 'GroupSuffix #'.$rowId.' not found');
+
+            return;
+        }
 
         $this->suffixId = $groupSuffix->id;
         $this->suffix = $groupSuffix->suffix;
         $this->server = $groupSuffix->server;
     }
 
-    public function updateGroupSuffix()
+    public function updateGroupSuffix(): void
     {
         $validatedData = $this->validate();
         $server = empty($validatedData['server']) ? '' : $validatedData['server'];
@@ -82,19 +78,27 @@ class ShowGroupSuffixes extends Component
         $this->closeModal('editGroupSuffixModal');
     }
 
-    public function deleteGroupSuffix(GroupSuffix $groupSuffix)
+    #[On('delete')]
+    public function deleteGroupSuffix($rowId): void
     {
+        $groupSuffix = GroupSuffix::find($rowId);
+        if ($groupSuffix == null) {
+            session()->flash('error', 'GroupSuffix #'.$rowId.' not found');
+
+            return;
+        }
+
         $this->suffixId = $groupSuffix->id;
         $this->suffix = $groupSuffix->suffix;
     }
 
-    public function delete()
+    public function delete(): void
     {
         GroupSuffix::find($this->suffixId)->delete();
         $this->resetInput();
     }
 
-    public function closeModal(?string $modalId = null)
+    public function closeModal(?string $modalId = null): void
     {
         $this->resetInput();
         if ($modalId != null) {
@@ -102,7 +106,7 @@ class ShowGroupSuffixes extends Component
         }
     }
 
-    private function resetInput()
+    private function resetInput(): void
     {
         $this->suffixId = null;
         $this->suffix = null;
@@ -111,8 +115,6 @@ class ShowGroupSuffixes extends Component
 
     public function render(): View
     {
-        $groupSuffixes = GroupSuffix::where('groupid', $this->group->id)->orderBy('id', 'ASC')->paginate(10);
-
-        return view('livewire.permissions.show-group-suffixes')->with('suffixes', $groupSuffixes);
+        return view('livewire.permissions.show-group-suffixes');
     }
 }
--- resources/views/livewire/permissions/show-group-suffixes.blade.php
+++ resources/views/livewire/permissions/show-group-suffixes.blade.php
@@ -1,69 +1,23 @@
 <div>
     @include('livewire.permissions.permission-group-suffixes-modals')
 
-    @if (session()->has('message'))
+    @if (session('message'))
         <h5 class="alert alert-success">{{ session('message') }}</h5>
     @endif
+    @if(session('error'))
+        <h5 class="alert alert-danger">{{ session('error') }}</h5>
+    @endif
 
-    <div class="card">
-        <div class="card-header h5 text-center mb-0">
-            <strong>Suffixes of {{ $group->name }}</strong>
+    <x-card-table title="Suffixes of {{ $group->name }}">
+        <livewire:permissions.group-suffixes-table groupId="{{ $group->id }}"/>
+    </x-card-table>
+    @can('edit_permissions')
+        <div class="p-4">
+            <button type="button" class="btn btn-primary" data-mdb-ripple-init data-mdb-modal-init
+                    data-mdb-target="#addGroupSuffixModal"
+                    wire:click="addGroupSuffix">
+                <i style="font-size: 18px !important;" class="material-icons">add</i> Add Suffix
+            </button>
         </div>
-        <div class="card-body border-0 shadow table-responsive">
-            <table class="table text-center">
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Suffix</th>
-                    <th>Server</th>
-                    @can('edit_permissions')
-                        <th>Actions</th>
-                    @endcan
-                </tr>
-                </thead>
-                <tbody>
-                @forelse($suffixes as $suffix)
-                    @php
-                        $server = $suffix->server;
-                        if (empty($server)) {
-                            $server = "ALL";
-                        }
-                    @endphp
-                    <tr>
-                        <td>{{ $suffix->id }}</td>
-                        <td>{{ $suffix->suffix }}</td>
-                        <td>{{ $server }}</td>
-                        @can('edit_permissions')
-                            <td>
-                                <button type="button" style="background: transparent; border: none;"
-                                        data-mdb-ripple-init data-mdb-modal-init
-                                        data-mdb-target="#editGroupSuffixModal"
-                                        wire:click="editGroupSuffix({{$suffix->id}})">
-                                    <i class="material-icons text-warning">edit</i>
-                                </button>
-                                <button type="button" style="background: transparent; border: none;"
-                                        data-mdb-ripple-init data-mdb-modal-init
-                                        data-mdb-target="#deleteGroupSuffixModal"
-                                        wire:click="deleteGroupSuffix({{ $suffix->id }})">
-                                    <i class="material-icons text-danger">delete</i>
-                                </button>
-                            </td>
-                        @endcan
-                    </tr>
-                @empty
-                    <tr>
-                        <td colspan="6" class="text-center">Sorry - No Data Found</td>
-                    </tr>
-                @endforelse
-                </tbody>
-            </table>
-            {{ $suffixes->links() }}
-        </div>
-    </div>
-    <div class="p-4">
-        <button type="button" class="btn btn-primary" data-mdb-ripple-init data-mdb-modal-init data-mdb-target="#addGroupSuffixModal"
-                wire:click="addGroupSuffix">
-            <i style="font-size: 18px !important;" class="material-icons">add</i> Add Suffix
-        </button>
-    </div>
+    @endcan
 </div>
