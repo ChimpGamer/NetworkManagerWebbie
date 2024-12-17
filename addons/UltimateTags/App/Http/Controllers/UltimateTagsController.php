e70bb35bea13875e90cb37afccb96c25d1d004e0
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-12-17 20:27:18 +0100

    Updated UltimateTags addon to use powergrid.

--- addons/UltimateTags/App/Http/Controllers/UltimateTagsController.php
+++ addons/UltimateTags/App/Http/Controllers/UltimateTagsController.php
@@ -8,6 +8,17 @@ use Illuminate\Http\Request;
 
 class UltimateTagsController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      */
--- addons/UltimateTags/resources/views/livewire/show-tags.blade.php
+++ addons/UltimateTags/resources/views/livewire/show-tags.blade.php
@@ -1,70 +1,16 @@
 <div>
     @include('ultimatetags::livewire.tags-modals')
 
-    @if (session()->has('message'))
+    @if (session('message'))
         <h5 class="alert alert-success">{{ session('message') }}</h5>
     @endif
-    @if (session()->has('error'))
+    @if (session('error'))
         <h5 class="alert alert-danger">{{ session('error') }}</h5>
     @endif
 
-    <div class="card">
-        <div class="card-header py-3">
-            <h5 class="mb-0 text-center">
-                <strong>Tags</strong>
-            </h5>
-
-            <div class="float-end d-inline" wire:ignore>
-                <div class="form-outline" data-mdb-input-init>
-                    <input type="search" id="tagsSearch" class="form-control" wire:model.live="search"/>
-                    <label class="form-label" for="tagsSearch"
-                           style="font-family: Roboto, 'FontAwesome'">Search...</label>
-                </div>
-            </div>
-        </div>
-
-        <div class="card-body border-0 shadow table-responsive">
-            <table class="table">
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Tag</th>
-                    <th>Description</th>
-                    <th>Permission</th>
-                    <th>Server</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($tags as $tag)
-                    <tr>
-                        <td>{{ $tag->id }}</td>
-                        <td>{{ $tag->name }}</td>
-                        <td>{!! $tag->tag !!}</td>
-                        <td>{{ $tag->description }}</td>
-                        <td>{{ $tag->permission }}</td>
-                        <td>{{ $tag->server }}</td>
-                        <td>
-                            <button type="button" style="background: transparent; border: none;"
-                                    data-mdb-ripple-init data-mdb-modal-init
-                                    data-mdb-target="#editTagModal" wire:click="editTag({{$tag->id}})">
-                                <i class="material-icons text-warning">edit</i>
-                            </button>
-                            <button type="button" style="background: transparent; border: none;"
-                                    data-mdb-ripple-init data-mdb-modal-init
-                                    data-mdb-target="#deleteTagModal" wire:click="deleteTag({{$tag->id}})">
-                                <i class="material-icons text-danger">delete</i>
-                            </button>
-                        </td>
-                    </tr>
-                @endforeach
-
-                </tbody>
-            </table>
-            {{ $tags->links() }}
-        </div>
-    </div>
+    <x-card-table title="Tags">
+        @livewire('ultimatetags::tags-table')
+    </x-card-table>
     <div class="p-4">
         <button type="button" class="btn btn-primary" data-mdb-ripple-init data-mdb-modal-init
                 data-mdb-target="#addTagModal"
