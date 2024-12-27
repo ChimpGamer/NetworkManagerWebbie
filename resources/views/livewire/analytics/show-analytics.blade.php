38f720c269a44c5c294bce544e8ab5b50d594051
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-12-27 20:54:43 +0100

    Updated Most Played Version and Most Used Virtual Hosts to use powergrid and fixed a few bugs.

--- resources/views/livewire/analytics/show-analytics.blade.php
+++ resources/views/livewire/analytics/show-analytics.blade.php
@@ -43,67 +43,15 @@
     </section>
 
     <section class="mb-4">
-        <div class="card">
-            <div class="card-header text-center py-3">
-                <h5 class="mb-0 text-center">
-                    <strong>Most Played Versions</strong>
-                </h5>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-hover text-nowrap">
-                        <thead>
-                        <tr>
-                            <th>Version</th>
-                            <th>Players</th>
-                            <th>Percentage</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($this->mostPlayedVersions as $mostPlayedVersion)
-                            <tr>
-                                <td>{{ $mostPlayedVersion['version'] }}</td>
-                                <td>{{ $mostPlayedVersion['players'] }}</td>
-                                <td>{{ $mostPlayedVersion['percentage'] }}%</td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+        <x-card-table title="Most Played Version">
+            <livewire:analytics.most-played-versions-table />
+        </x-card-table>
     </section>
 
     <section class="mb-4">
-        <div class="card">
-            <div class="card-header text-center py-3">
-                <h5 class="mb-0 text-center">
-                    <strong>Most Used Virtual Hosts</strong>
-                </h5>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-hover text-nowrap">
-                        <thead>
-                        <tr>
-                            <th>Virtual Host</th>
-                            <th>Players</th>
-                            <th>Population</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($this->mostUsedVirtualHosts as $mostUsedVirtualHost)
-                            <tr>
-                                <td>{{ $mostUsedVirtualHost['vhost'] }}</td>
-                                <td>{{ $mostUsedVirtualHost['players'] }}</td>
-                                <td>{{ $mostUsedVirtualHost['percentage'] }}%</td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+        <x-card-table title="Most Used Virtual Hosts">
+            <livewire:analytics.most-used-virtual-hosts-table />
+        </x-card-table>
     </section>
 </div>
 
