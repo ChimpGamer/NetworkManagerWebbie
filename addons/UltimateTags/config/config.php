be0a0330730ee1bfe4ef9ca914d4e92541c9c38a
ChimpGamer <Tim.Cremers@Ziggo.nl>
2025-04-12 21:41:57 +0200

    Restore default UltimateTags config

--- addons/UltimateTags/config/config.php
+++ addons/UltimateTags/config/config.php
@@ -5,11 +5,11 @@ return [
 
     'storage' => [
         'driver' => 'mysql',
-        'host' => 'st02.cremers.local',
-        'port' => 3307,
+        'host' => 'localhost',
+        'port' => 3306,
         'database' => 'UltimateTags',
-        'username' => 'NetworkManager',
-        'password' => 'NetworkManager',
+        'username' => 'UltimateTags',
+        'password' => 'UltimateTags',
         'charset' => 'utf8mb4',
         'collation' => 'utf8mb4_unicode_ci',
         'options' => extension_loaded('pdo_mysql') ? array_filter([
