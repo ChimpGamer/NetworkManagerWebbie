efd5f0ecb78902f5b4475205da16c50d86db2a4c
ChimpGamer <Tim.Cremers@Ziggo.nl>
2025-05-26 13:14:38 +0200

    Redesigned the language messages page

--- app/Livewire/Languages/ShowLanguage.php
+++ app/Livewire/Languages/ShowLanguage.php
@@ -3,7 +3,6 @@
 namespace App\Livewire\Languages;
 
 use App\Models\Language;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\View\View;
 use Livewire\Component;
 
@@ -11,50 +10,8 @@ class ShowLanguage extends Component
 {
     public Language $language;
 
-    public Collection $languageMessages;
-
-    public string $search = '';
-
-    protected function rules(): array
-    {
-        return [
-            'languageMessages.*.key' => 'required|string',
-            'languageMessages.*.message' => 'string',
-        ];
-    }
-
-    public function mount(): void
-    {
-        $this->languageMessages = $this->language->languageMessages;
-    }
-
-    public function save(): void
-    {
-        $this->authorize('edit_languages');
-        $this->validate();
-
-        $changedMessages = $this->languageMessages->filter(function ($message) {
-            return $message->isDirty();
-        })->values();
-        if ($changedMessages->isEmpty()) {
-            return;
-        }
-        foreach ($changedMessages as $message) {
-            $message->save();
-        }
-        $message = 'Successfully updated the following message(s): '.$changedMessages->implode('key', ', ');
-        session()->flash('message', $message);
-    }
-
     public function render(): View
     {
-        $languageMessages = $this->languageMessages;
-
-        if (!empty($this->search)) {
-            $languageMessages = $languageMessages->filter(function ($message) {
-                return str_contains($message->key, $this->search);
-            });
-        }
-        return view('livewire.languages.show-language')->with('language', $this->language)->with('messages', $languageMessages);
+        return view('livewire.languages.show-language', ['language' => $this->language]);
     }
 }
--- resources/views/livewire/languages/show-language.blade.php
+++ resources/views/livewire/languages/show-language.blade.php
@@ -1,55 +1,7 @@
 <div>
-    <div class="col-lg-12">
-        <div class="card">
-            <div class="card-header">
-                <div class="row mt-2 align-items-center text-center">
-                    <div class="col-md-auto me-auto">
-                        <h4 class="mb-0">Language {{$language->name}}</h4>
-                    </div>
+    <h5 class="alert alert-info"><i class="fa-solid fa-circle-info"></i> Don't forget to press Enter to save the message. </h5>
 
-                    <div class="col-md-auto" x-init>
-                        <div class="col-md-auto ms-auto" wire:ignore x-data="{
-                            handleKeydown(e) {
-                                // 'cmd+k'
-                                if (e.keyCode == 75 && (e.metaKey == true || e.ctrlKey == true)) {
-                                    document.getElementById('languageMessageSearch').focus();
-                                    e.preventDefault()
-                                }
-
-                                // 'esc'
-                                if (e.keyCode == 27) {
-                                    document.getElementById('languageMessageSearch').blur();
-                                    e.preventDefault()
-                                }
-                            }
-                        }">
-                            <div class="form-outline w-auto d-inline-block" data-mdb-input-init>
-                                <input type="search" id="languageMessageSearch" class="form-control form-control-sm" @keydown.window="handleKeydown" wire:model.live="search"/>
-                                <label class="form-label" for="languageMessageSearch" style="font-family: Roboto, 'FontAwesome'">Search...</label>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card-body">
-                <form wire:submit="save">
-                    @foreach($messages as $i => $message)
-                        <div class="row" wire:key="message-field-{{ $message->key }}">
-                            <label for="message-label-{{ $message->key }}" class="col-sm-3 form-label">{{ $message->key }}</label>
-                            <div class="col-sm-9">
-                                <input id="message-label-{{ $message->key }}" class="form-control" type="text" wire:model="languageMessages.{{ $i }}.message">
-                            </div>
-                        </div>
-                        <div class="border-bottom my-3 border-gray-200"></div>
-                    @endforeach
-
-                    <button type="submit" class="btn btn-primary">Save</button>
-                </form>
-            </div>
-        </div>
-    </div>
-
-    @if (session('message'))
-        <h5 class="alert alert-success">{{ session('message') }}</h5>
-    @endif
+    <x-card-table title="Language {{$language->name}}">
+        <livewire:languages.language-messages-table :$language />
+    </x-card-table>
 </div>
