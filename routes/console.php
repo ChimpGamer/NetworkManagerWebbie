33833640405b065ed7d408bfcbd29b52ac6222dd
ChimpGamer <19960733+ChimpGamer@users.noreply.github.com>
2026-08-09 14:58:23 +0200

    Disable laravel's default migration commands because there are no migrations and could destory your database.

--- routes/console.php
+++ routes/console.php
@@ -1,1 +1,16 @@
 <?php
+
+// Disable Laravel's default migration commands
+collect([
+    'migrate',
+    'migrate:fresh',
+    'migrate:install',
+    'migrate:refresh',
+    'migrate:reset',
+    'migrate:status',
+    'migrate:rollback',
+])->each(function ($command) {
+    Artisan::command($command, function () use ($command) {
+        $this->comment("⚠️  {$command} command is disabled.");
+    })->describe("{$command} command is disabled.");
+});
